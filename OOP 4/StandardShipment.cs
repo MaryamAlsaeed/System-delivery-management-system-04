a173c38f5ff18d94373178f0b5ee01d7b2a0a978
Maryam <mariamsaeedalshamy@gmail.com>
2026-09-23 01:12:04 +0400

    ITrackable interface

---
+++ OOP 4/StandardShipment.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_4
+{
+    internal class StandardShipment : Shipment
+    {
+        public override double EstimatedCost
+        {
+            get { return DeliveryFee + (Weight * 5); }
+        }
+        public override void PrintShipment()
+        {
+            Console.WriteLine($"Tracking code: {TrackingCode}");
+            Console.WriteLine($"Description: {Description}");
+            Console.WriteLine($"Weight: {Weight} KG");
+            Console.WriteLine($"Delivery Fee: {DeliveryFee}");
+            Console.WriteLine($"Destination: {Destination.GetFullAddress()}");
+            Console.WriteLine($"Estimated Cost: {EstimatedCost}");
+        }
+        public StandardShipment(string trackingCode, string description, double weight, double deliveryFee, DeliveryAddress destination)
+            : base(trackingCode, description, weight, deliveryFee, destination)
+        {
+        }
+    }
+}
