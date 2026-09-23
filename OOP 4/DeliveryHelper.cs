e1ac45690ca0e4fc0df4461a5bb7431f1a4299a0
Maryam <mariamsaeedalshamy@gmail.com>
2026-09-23 17:23:00 +0400

    Delivery Helper class

---
+++ OOP 4/DeliveryHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_4
+{
+    public static class DeliveryHelper
+    {
+        public static void PrintShipmentDetails(Shipment shipment)
+        {
+            if (shipment != null)
+            {
+                shipment.PrintShipment();
+                Console.WriteLine();
+            }
+        }
+    }
+}
